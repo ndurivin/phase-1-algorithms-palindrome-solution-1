fd2629c2d476d9ebfc5668d5a0cd689309cca24c
Vinn Nduri <ndurivo@gmail.com>
2022-06-19 09:40:58 +0300

    commit tree f242813f47430f2a0c131c6fecadfe726355ba31

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+function createReverse(word){
+  
+  const reversedWord = word.split("").reverse().join("")
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+ const reversedWord = createReverse(word)
+ 
+return reversedWord === word
 }
+
 
 /* 
   Add your pseudocode here
+  reverse the string input
+  Compare the word and the reversed version
+  if same return true, if not return false
 */
-
 /*
   Add written explanation of your solution here
+  write the function that accepts the string as input
+  in JS, .reverse only works in arrays and so the word is  slit, reversed then later joined using function createReverse
+  the reversed string is then compared against the input
+  If the two compare both forward and in reverse, its a palindrome hence return true, if not return false
+
 */
 
 // You can run `node index.js` to view these console logs
